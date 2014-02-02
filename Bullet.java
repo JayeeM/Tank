4905bcdefe38db80ff9cca0d5123c6181e1e26d7
JayeeM <pearllycrystals@gmail.com>
2014-02-01 19:42:59 -0800

    Create Bullet.java

---
+++ Bullet.java
@@ -0,0 +1,20 @@
+
+public class Bullet {
+int x;
+int y;
+int direction;
+public Bullet(int x, int y, int d){
+	this.x=x;
+	this.y=y;
+	this.direction=d;
+}
+public void move(){
+	if(direction==0){
+		
+	}
+	else if()
+}
+public void draw(){
+	
+}
+}
